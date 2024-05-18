0d288596fe3e227d5952dc38b9de6b3c41ba964e
Thiago Serpa <151270129+ThiagoMLS@users.noreply.github.com>
2024-05-18 18:26:09 -0300

    Pomodoro Timer

---
+++ Desafio Anula Multa Front-End/Atualização 2 - Front-End/pages/create.js
@@ -0,0 +1,78 @@
+import "../src/app/globals.css";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const Create = () => {
+  const router = useRouter();
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+
+  const onChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+
+  const handleCreate = async () => {
+    const response = await fetch("/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    if (response.ok) {
+      // Task created successfully
+      router.push("/");
+    } else {
+      // Handle error
+      alert("Failed to create task");
+    }
+  };
+
+  return (
+    <>
+      <div className="container mx-auto mt-8 max-w-[560px]">
+        <div className="flex justify-between items-center pb-4 border-b border-dashed border-gray-900 mb-4">
+          <h1 className="text-3xl font-semibold">Criar Tarefa</h1>
+        </div>
+        <form>
+          <div className="mb-4">
+            <label>Título</label>
+            <input
+              className="mt-1 px-4 py-2 border border-gray-300 rounded-md block w-full"
+              type="text"
+              name="title"
+              value={task?.title}
+              onChange={onChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label>Descrição</label>
+            <input
+              className="mt-1 px-4 py-2 border border-gray-300 rounded-md block w-full"
+              type="text"
+              name="description"
+              value={task?.description}
+              onChange={onChange}
+            />
+          </div>
+          <button
+            className="bg-green-600 hover:bg-opacity-80 text-white rounded-lg px-4 py-2 duration-200 w-full"
+            type="button"
+            onClick={handleCreate}
+          >
+            Criar tarefa
+          </button>
+        </form>
+      </div>
+      <Head>
+        <title>Criar tarefa</title>
+      </Head>
+    </>
+  );
+};
+
+export default Create;
---
+++ Desafio Anula Multa Front-End/Atualização 2 - Front-End/pages/index.js
@@ -0,0 +1,141 @@
+import { useState, useEffect } from "react";
+import "../src/app/globals.css";
+import Link from "next/link";
+
+
+
+const linkStyle = {
+    marginRight: 15
+}
+
+const Menu = () => {
+    const [tasks, setTasks] = useState([]);
+    const [mode, setMode] = useState('work'); 
+    const [time, setTime] = useState(25 * 60);
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        let interval = null;
+        if (isActive && time > 0) {
+            interval = setInterval(() => {
+                setTime(prevTime => prevTime - 1);
+            }, 1000);
+        } else if (!isActive && time !== 0) {
+            clearInterval(interval);
+        } else if (isActive && time === 0) {
+            if (mode === 'work') {
+                setMode('rest');
+                setTime(5 * 60); 
+            } else {
+                setMode('work');
+                setTime(25 * 60); 
+            }
+        }
+        return () => clearInterval(interval);
+    }, [isActive, time, mode]);
+
+    const toggleTimer = () => {
+        setIsActive(!isActive);
+    };
+
+    const resetTimer = () => {
+        if (mode === 'work') {
+            setTime(25 * 60);
+        } else {
+            setTime(5 * 60);
+        }
+        setIsActive(false);
+    };
+
+    const addTime = (additionalTime) => {
+        setTime(prevTime => prevTime + additionalTime);
+    };
+
+     const formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        return (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    };
+
+    useEffect(() => {
+      const fetchTasks = async () => {
+        const response = await fetch("/api/tasks");
+        const data = await response.json();
+        setTasks(data);
+      };
+  
+      fetchTasks();
+    }, []);
+
+    return (
+        <div>
+           
+            <div className="pomodoro-container">
+                <h1>POMODORO</h1>
+                <div className="timer">{formatTime(time)}</div>
+                <div className="controls">
+                    <div className="button-container">
+                        <div className="button-container">
+                          <button className={"btn " + (isActive ? 'pause-btn' : 'start-btn')} onClick={toggleTimer} id="button">
+                            {isActive ? 'Pausar' : 'Começar'}
+                            </button>
+                    </div>
+
+                    
+
+                    <div className="button-container">
+                        <button className="btn pause-btn" onClick={resetTimer} id="button">
+                            Reiniciar
+                        </button>
+                    </div>
+                    <div className="button-container">
+                    <button className="btn add-time-btn" onClick={() => addTime(60)}>
+                        + Add 1 Minute
+                    </button>
+                    </div>
+
+                </div>
+                </div>
+            </div>
+
+
+            <div className="quadro-tasks">
+
+            <div className="flex justify-between items-center pb-4 border-b border-dashed border-gray-900 mb-4">
+            <h1 className="text-3xl font-semibold">TAREFAS</h1>
+
+            <Link
+                className="bg-green-600 hover:bg-opacity-80 text-white rounded-lg px-4 py-2 duration-200"
+                href="/create"
+              >
+                Criar nova tarefa 
+            </Link>
+
+            </div>
+            <ul>
+            {tasks.map((task) => (
+              <li key={task.id} className="py-2 flex justify-between w-full">
+            <span>
+            <strong>{task.title}</strong> - {task.description}
+            </span>
+            <span className="flex gap-2">
+           <Link className="btn" href={`/${task.id}/edit`}>Editar</Link>
+            <Link className="btn" href={`/${task.id}/delete`}>Deletar</Link>
+            </span>
+            </li>
+  ))}
+</ul>
+
+</div>
+
+
+            
+
+
+            
+
+        </div>
+    );
+};
+
+export default Menu;
